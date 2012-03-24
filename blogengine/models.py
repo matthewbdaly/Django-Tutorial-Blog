41df1c78ccd2975c863aac7279d0ec8cf9cb6b87
Matthew Daly <matthewbdaly@gmail.com>
2012-03-24 19:34:24 +0000

    Added a model for categories, and added a ManyToMany field for categories to Posts.

--- blogengine/models.py
+++ blogengine/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=40, unique=True)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/categories/%s/" % self.slug
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
     slug = models.SlugField(max_length=40, unique=True)
     author = models.ForeignKey(User)
+    categories = models.ManyToManyField(Category)
 
     def __unicode__(self):
         return self.title
