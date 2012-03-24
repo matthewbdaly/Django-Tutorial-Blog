ab4848ad19e2205505d14ea7244ac3d19b0393df
Matthew Daly <matthewbdaly@gmail.com>
2012-03-24 20:17:44 +0000

    Added the capability to add categories to posts.

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -2,9 +2,14 @@ import models
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+class CategoryInline(admin.TabularInline):
+    model = models.Post.categories.through
+    extra = 0
+
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     exclude = ('author',)
+    inlines = [CategoryInline]
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
